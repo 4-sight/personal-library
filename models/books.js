1fd16ef18cfb4688b5194117d1d583f0b81fa0b3
Peter Newton <peter@4sightdesigns.com>
2018-12-23 15:44:22 +0000

    Add books model methods

--- models/books.js
+++ models/books.js
@@ -3,9 +3,46 @@ const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema({
   title: String,
-  commentcount: Number
+  comments: [String]
 })
 
 const booksModel = mongoose.model('books', bookSchema)
+
+booksModel.addComment = function(id, comment) {
+
+  return this.findByIdAndUpdate(
+    id,
+    {$push: {comments: comment}}
+  )
+}
+
+
+booksModel.addBook = function(title) {
+
+  return this.create({
+    title: title,
+    comments: []
+  })
+}
+
+
+booksModel.deleteBook = function(id) {
+
+  return this.deleteOne({
+    _id: id
+  })
+}
+
+
+booksModel.deleteAllBooks = function() {
+
+  this.deleteMany({})
+}
+
+
+booksModel.getAllBooks = function() {
+
+  return this.find()
+}
 
 module.exports = booksModel
